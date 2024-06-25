efe09965dd8df3b5f41535d8a866b0581e360c41
David Kadish <david.kadish@gmail.com>
2024-06-25 15:17:02 +0200

    Working sd card and battery sensor
    Though the battery sensor's values are strange...

--- firmware/bait2/src/main.cpp
+++ firmware/bait2/src/main.cpp
@@ -3,14 +3,15 @@
 #include "sdcard.h"
 #include "logging.h"
 #include "PowerSensor.h"
-#include "OLEDDisplay.h"
-#include "FFTReader.h"
+// #include "OLEDDisplay.h"
+// #include "FFTReader.h"
 
-PowerSensor powerSensor = PowerSensor(10, "power.csv", 6600);
+PowerSensor powerSensor = PowerSensor(10, "power.csv", 4400);
 
-OLEDDisplay  display = OLEDDisplay();
+// OLEDDisplay display = OLEDDisplay();
 
-void setup() {
+void setup()
+{
 #ifdef USE_STATUS_LED
     // Indicator LED
     pinMode(LED_BUILTIN, OUTPUT);
@@ -19,12 +20,18 @@ void setup() {
     Serial.begin(115200);
 
 #ifdef WAIT_FOR_SERIAL
-    while(!Serial){}
+    for (int i = 0; i < 10 && !Serial; i++)
+    {
+        Serial.begin(115200);
+        delay(100);
+        DEBUG("Waiting for Serial...")
+    }
 #endif
     delay(1000);
 
-#ifdef  USE_STATUS_LED
-    for(int i=0; i<5; i++){
+#ifdef USE_STATUS_LED
+    for (int i = 0; i < 5; i++)
+    {
         LEDHIGH
         delay(500);
         LEDLOW
@@ -40,61 +47,16 @@ void setup() {
     DEBUG("SD Initialised.")
 
     powerSensor.setup();
-    display.setup();
+    // display.setup();
 
     DEBUG("Setup Complete.")
 }
 
-int v=0;
-void oledLoop(){
-    int line = 0;
-
-    display.myOLED.erase();
-
-    //Time
-    char str[48];
-    sprintf(str, "%d%c%d %02d:%02d", year() % 100, (char) ('A' - 1 + month()), day(), hour(), minute());
-    display.myOLED.text(0, line, str);
-//    line += 8;
-//    sprintf(str, "%d:%02d:%02d", hour(), minute(), second());
-//    display.myOLED.text(0, line, str);
-
-    line += 8;
-    display.myOLED.line(0,line,display.myOLED.getWidth(), line);
-    line += 2;
-
-    //Battery Info
-    display.myOLED.text(0,line, String(powerSensor.soc, 0) + "%");
-//    display.myOLED.text(0,8, String(powerSensor.volts, 0) + "mV");
-    display.myOLED.text(28,line, "0000mA");
-//    display.myOLED.text(0,16, String(powerSensor.current, 0) + "mA");
-//    display.myOLED.text(0,24, String(powerSensor.capacity, 0) + "mAh");
-    line += 8;
-    display.myOLED.text(0,line, "0000mW");
-//    display.myOLED.text(0,32, String(powerSensor.power, 0) + "mW");
-//    display.myOLED.text(0,40, String(powerSensor.health, 0) + "%");
-
-    line += 8;
-    display.myOLED.line(0,line,display.myOLED.getWidth(), line);
-    line += 2;
-
-    // SD Card
-    if(powerSensor.getStatus() == 0){
-        display.myOLED.text(0,line, "SD OK");
-    } else {
-        display.myOLED.text(0,line, "SD FAIL");
-    }
-
-    // Working animation
-    display.myOLED.pixel(v, display.myOLED.getHeight()-1);
-    v += 1;
-    v %= display.myOLED.getWidth();
-
-    display.myOLED.display();  // show the added char
-}
-
-void loop() {
+int v = 0;
+// ̆̆
+void loop()
+{
     powerSensor.loop();
 
-    oledLoop();
+    // oledLoop();
 }
