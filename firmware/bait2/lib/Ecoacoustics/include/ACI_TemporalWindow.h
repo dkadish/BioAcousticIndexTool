6ca2eace92ae4c94d9dbc5589813ef61bcd73e2b
David Kadish <david.kadish@gmail.com>
2024-07-01 20:02:09 +0200

    ACI seems to be working. Should be tested

--- firmware/bait2/lib/Ecoacoustics/include/ACI_TemporalWindow.h
+++ firmware/bait2/lib/Ecoacoustics/include/ACI_TemporalWindow.h
@@ -5,15 +5,14 @@
 #ifndef SBAT_ACI_TEMPORALWINDOW_H
 #define SBAT_ACI_TEMPORALWINDOW_H
 
-
 #include "Sensor.h"
 #include "FFTReader.h"
 #include <float.h>
 
-class ACI_TemporalWindow : public Sensor {
+class ACI_TemporalWindow : public Sensor
+{
 public:
-    ACI_TemporalWindow(int interval, FFTReader &fft);
-    ACI_TemporalWindow(int interval, FFTReader &fft, bool doFilter, bool doDiscardAdjacentZeros);
+    ACI_TemporalWindow(int interval, FFTReader &fft, bool doFilter = false, bool doDiscardAdjacentZeros = false, int debugInterval = 0);
 
     void setup() override;
 
@@ -23,39 +22,43 @@ public:
 
     void process() override;
 
-    void zero(){ aci = 0.0; _count = 0; _fft.resetCount(); }
+    void debug() override;
 
-    float getValue(){ return aci; }
+    void zero()
+    {
+        aci = 0.0;
+        _count = 0;
+        _fft.resetCount();
+    }
 
-    int getCount(){ return _count; };
+    float getValue() { return aci; }
 
-    int getFFTCount(){ return _fft.getCount(); };
+    int getCount() { return _count; };
 
-    void setFilter(bool doFilter){ _filter_fft = doFilter; };
+    int getFFTCount() { return _fft.getCount(); };
 
-    void setFilterLimit(float filterLimit){ _filter_below = filterLimit; };
+    void setFilter(bool doFilter) { _filter_fft = doFilter; };
 
-    void setDiscardAdjacentZeros(bool doDiscardAdjacentZeros){ _discard_adjacent_zeros = doDiscardAdjacentZeros; };
+    void setFilterLimit(float filterLimit) { _filter_below = filterLimit; };
+
+    void setDiscardAdjacentZeros(bool doDiscardAdjacentZeros) { _discard_adjacent_zeros = doDiscardAdjacentZeros; };
 
 private:
-
-    float D[FFTReader::length]; // The sum of binwise differences between timesteps, for each bin
-    float I[FFTReader::length]; // The sum of intesities for each bin
+    float D[FFTReader::length];   // The sum of binwise differences between timesteps, for each bin
+    float I[FFTReader::length];   // The sum of intesities for each bin
     float I_k[FFTReader::length]; // The previous intesity for each bin
     bool _first = true;
 
-    bool _filter_fft = false; // Should FFT values be set to zero when below a threshold? See Farina et. al. (2016). The Application of ...
+    bool _filter_fft;                        // Should FFT values be set to zero when below a threshold? See Farina et. al. (2016). The Application of ...
     float _filter_below = FLT_MIN + FLT_MIN; // Set the default filter value to be twice the Minimum normalized positive floating-point number
 
-    bool _discard_adjacent_zeros = false; // Ignore frame differences when one of the two frames is zero. See  Farina et. al. (2016).
+    bool _discard_adjacent_zeros; // Ignore frame differences when one of the two frames is zero. See  Farina et. al. (2016).
 
     int _count = 0;
 
     float aci;
 
     FFTReader &_fft;
-
 };
 
-
-#endif //SBAT_ACI_TEMPORALWINDOW_H
+#endif // SBAT_ACI_TEMPORALWINDOW_H
--- firmware/bait2/src/main.cpp
+++ firmware/bait2/src/main.cpp
@@ -5,10 +5,14 @@
 #include "lora.h"
 #include "PowerSensor.h"
 // #include "OLEDDisplay.h"
-#include "FFTReader.h"
 #include "BME680.h"
 #include "LightSensor.h"
 #include "RMS.h"
+
+// Spectral Audio
+#include "FFTReader.h"
+#include "ACI_TemporalWindow.h"
+#include "AcousticComplexityIndex.h"
 
 #include <Audio.h>
 #include <SPI.h>
@@ -51,10 +55,15 @@ float gain_l = 32.0; // Seems to be a good default value
 LoRaWANTTN lora = LoRaWANTTN();
 
 PowerSensor powerSensor = PowerSensor(5L * 60L, "/power.csv", &lora, 4400);
-FFTReader fftReader = FFTReader(fft256_l, "/fft.csv", false, 2, -1);
 EnvironmentalSensor envSensor = EnvironmentalSensor("/env.csv", &lora, 5 * 60);
 LightSensor lightSensor = LightSensor("/light.csv", &lora, 5 * 60);
+
 RootMeanSquare rms = RootMeanSquare(rms_l, "/rms.csv", &lora, 5L * 60L, 0, -1);
+
+// Spectral Audio
+FFTReader fftReader = FFTReader(fft256_l, "/fft.csv", false, 2, -1);
+ACI_TemporalWindow aci_window = ACI_TemporalWindow(30, fftReader, false, false, 0); // IS THIS RIGHT PARAMETERS?
+AcousticComplexityIndex aci = AcousticComplexityIndex(15L * 60L, aci_window, "/aci.csv", &lora);
 
 // OLEDDisplay display = OLEDDisplay();0
 
@@ -110,10 +119,13 @@ void setup()
     lora.setup();
     powerSensor.setup();
     // display.setup();
-    fftReader.setup();
     envSensor.setup();
     lightSensor.setup();
+
     rms.setup();
+
+    fftReader.setup();
+    aci_window.setup();
 
     DEBUG("Setup Complete.")
 }
@@ -123,10 +135,13 @@ int v = 0;
 void loop()
 {
     powerSensor.loop();
-    fftReader.loop();
     envSensor.loop();
     lightSensor.loop();
+
     rms.loop();
+
+    fftReader.loop();
+    aci_window.loop();
 
     // oledLoop();
 
